349eb937a0dd15f33de10702828306ad0a751605
Mrlai <1433054819@qq.com>
2019-12-09 13:56:06 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Category from '../views/Category.vue'
+import City from '../views/City.vue'
+import Detail from '../views/Detail.vue'
+import Home from '../views/Home.vue'
+import login from '../views/Login.vue'
 Vue.use(VueRouter)
 
-const routes = []
+// const routes = []
 
 const router = new VueRouter({
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Category',
+      name: 'Category',
+      component: Category
+    },
+    {
+      path: '/City',
+      name: 'City',
+      component: City
+    },
+    {
+      path: '/Detail',
+      name: 'Detail',
+      component: Detail
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
 })
 
 export default router
---
+++ src/views/City.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>城市定位页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
